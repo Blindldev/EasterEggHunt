49f75b9062a24d37a54089bd54625474c2c1e77e
Michael Vicente <mvicente@Michaels-Air.lan>
2025-04-20 10:40:15 -0500

    Revert to working configuration from 9 hours ago

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
-  server: {
-    port: 5173,
-    strictPort: true,
-    open: true
-  },
+  base: '/EasterEggHunt/',
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -23,5 +18,9 @@ export default defineConfig({
         }
       }
     }
+  },
+  server: {
+    port: 5173,
+    strictPort: true
   }
 }) 
