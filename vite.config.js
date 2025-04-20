03e356fbed15964c5366695e4a5dac07da70de88
Michael Vicente <mvicente@Michaels-Air.lan>
2025-04-20 11:40:04 -0500

    Fix MIME type issues and file paths

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,9 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
   }
 }) 
